d192eb6c857f3642b036d18715354131e91d21f1
Ahdab Serhan <ms.ahdab@gmail.com>
2019-08-06 16:19:15 +0300

    added server and router and handlers and index.html and app.js and mean.css files

---
+++ src/database/db_connection.js
@@ -0,0 +1,3 @@
+const { Pool } = require('pg');
+const url = require('url');
+require('env2')('./config.env');
---
+++ src/router.js
@@ -0,0 +1,22 @@
+const {
+    homeHandler,
+    
+    publicHandler,
+    errorHandler
+  } = require('./handlers');
+  
+  const router = (request, response) => {
+    const { url } = request;
+  
+    if (url === '/') {
+      homeHandler(response);
+    
+    } else if (url.includes('public')) {
+      publicHandler(url, response);
+    } else {
+      errorHandler(response);
+    }
+  };
+  
+  module.exports = router;
+  
